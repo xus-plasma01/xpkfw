39eb482a64223d579ca69b979d3ea7c4af25bd0e
xus_plasma1 <xus.plasma@gmail.com>
2026-09-04 20:46:04 +0100

    Update main.c

--- main.c
+++ main.c
@@ -3,19 +3,17 @@
 #include "xpk.h"
 
 int main(void) {
-  xpkWindow *window = xpkCreateWindow(800, 600, "thing");
-  if (!window) {
-    fprintf(stderr, "%c", xpkGetError());
-    xpkDeleteWindow(window);
-    xpkEnd();
-  }
+	xpkWindow *window = xpkCreateWindow(800, 600, "xpk [core] example - basic window");
+	if (!window) {
+		fprintf(stderr, "%c", xpkGetError();
+		xpkDeleteWindow(window);
+		xpkEnd();
+	}
 
-	xpkOpenWindow(window, SW_SHOW);
+	while (!xpkWindowShouldClose(window)) {
+		xpkWaitFrames(window);
+		xpkSwapFrames(window);
+	}
 
-  while (!xpkWindowShouldClose(window)) {
-    xpkWaitEvents(window);
-    xpkSwapFrames(window);
-  }
-
-  xpkEnd();
+	xpkEnd();
 }
