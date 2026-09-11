b20b11f14efe9ef363b75c975503cafe0ba263f6
xus_plasma1 <xus.plasma@gmail.com>
2026-09-11 21:50:36 +0100

    Refactor window rendering loop for consistency

--- main.c
+++ main.c
@@ -9,18 +9,18 @@ int main(void) {
 	xpkWindow *w3 		= xpkBeginWindow(500, 500, 800, 600, "w3");
 
   while (1) {
-    xpkSpinWindow(window);
-    xpkSwapWindow(window);
+    	xpkSpinWindow(window);
+    	xpkSwapWindow(window);
 
-    xpkSpinWindow(win2);
-    xpkSwapWindow(win2);
+    	xpkSpinWindow(win2);
+    	xpkSwapWindow(win2);
 
-    xpkSpinWindow(w3);
-    xpkSwapWindow(w3);
-  }
+    	xpkSpinWindow(w3);
+    	xpkSwapWindow(w3);
+	}
 
 	xpkStopWindow(window);
 	xpkStopWindow(win2);
 	xpkStopWindow(w3);
-  xpkEnd();
+	xpkEnd();
 }
